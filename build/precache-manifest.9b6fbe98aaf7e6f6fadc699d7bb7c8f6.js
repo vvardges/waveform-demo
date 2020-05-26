dc16206db2c1ad41718dad42c09912fa56baa8ed
Vardges Vardanyan <vako3008@gmail.com>
2020-05-26 04:34:37 +0400

    initialize project

---
+++ build/precache-manifest.9b6fbe98aaf7e6f6fadc699d7bb7c8f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d775c5a9d3f055775bfd6a315d3bc6e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fe0a934df54b2d7e44c",
+    "url": "/static/css/main.06b7f34d.chunk.css"
+  },
+  {
+    "revision": "2e0ed218121ccf1b865e",
+    "url": "/static/js/2.ad27ef40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad27ef40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe0a934df54b2d7e44c",
+    "url": "/static/js/main.15782633.chunk.js"
+  },
+  {
+    "revision": "28ef7db1da4fb6095358",
+    "url": "/static/js/runtime-main.5cbec568.js"
+  }
+]);
